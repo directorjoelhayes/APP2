590a4b3cf764a30f7ed480080d4fec890461056f
NicolasGuil <62348885+NicolasGuil@users.noreply.github.com>
2020-04-10 18:16:07 +0200

    update cube

--- src/Berekeningen/Cube.js
+++ src/Berekeningen/Cube.js
@@ -1,15 +1,11 @@
-import React, { Component, Children } from "react";
+import './Cube.css';
+import React, { Component} from 'react';
 import ReactDOM from "react-dom";
 import * as THREE from "three";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { PhotoshopPicker } from 'react-color'
-import './Cube.css'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
 import {connect} from 'react-redux'
 
-var myArray = ['str', 'iso', 'afw']
-var testingMyCubes = [];
-var scene = '';
-var myCubes = [];
 
 class Cube extends Component {
   constructor(props) {
@@ -24,56 +20,118 @@ class Cube extends Component {
       colorTemp : '#ff0000',
       colorModal : 'closed'
     }
-    this.listItems = (cubes) => {
-      return cubes.map((cube,index) =>
-        <div className="box" key={index}>{cube.name}</div>
-      );
-    }
+
+ 
+//kubussen oplijsten
+  this.listItems = (cubes) => {
+    return cubes.map((cube,index) =>
+  <div className='box' key={index}>{cube.name}</div>
+    );
+  }
+}
+
+
+
+handleChange(event) {
+  this.setState({value: event.target.value});
+}
+
+
+//De eigenschappen van de kubus bepalen, volgens three js, colorpicker, breedte
+
+addCube(){
+  
+  var layerList = this.props.layerListdata
+  var myCubes = []
+
+  // //reset scene from cubes
+
+  this.scene = new THREE.Scene()
+  var canvasWidth = document.getElementById('canvas').clientWidth;
+  var camera = new THREE.PerspectiveCamera( 75, canvasWidth/window.innerHeight, 0.1, 1000);
+  var renderer = new THREE.WebGLRenderer();
+
+  var light = new THREE.HemisphereLight( 0xffffff, 0x080820, .5);
+  this.scene.add( light);
+  var ambientLight = new THREE.AmbientLight(0xffffff, .5);
+  this.scene.add( ambientLight );
+  var directionalLight = new THREE.DirectionalLight( 0xffffff, .3 );
+  this.scene.add( directionalLight );
+
+  directionalLight.position.set(0,0,200);
+
+  renderer.setSize( canvasWidth, window.innerHeight );
+
+  //orbit-controles
+
+  var controls = new OrbitControls( camera, renderer.domElement );
+
+  camera.position.set( -2, 2, 6 );
+  controls.update();
+
+  // this.mount.appendChild ( renderer.domElement ); -> weg want anders dubbel 3D-view...
+
+  camera.position.z = 150;
+  var animate =() => {
+    requestAnimationFrame( animate );
+    controls.update();
+    renderer.render( this.scene, camera );
+  };
+  animate();
+
+  
+//make cube for every layer
+  layerList.map((layer,index)=>{
+  
+  var geometry = new THREE.BoxGeometry();
+  var material = new THREE.MeshPhongMaterial({color: layer.kleur}); //color: layer.kleur -> nog toe te voegen in array
+  var cube = new THREE.Mesh ( geometry, material );
+  let newWidth = layer.dikte;
+
+  cube.scale.x = newWidth;
+  cube.scale.y = 100;
+  cube.scale.z = 100;
+  let newPosition = 0;
+  if(myCubes.length > 0){
+    let lastBox = myCubes.length -1;
+    newPosition = myCubes[lastBox].position.x + (myCubes[lastBox].scale.x/2) + (newWidth/2);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleColorModalOpen = () => {
-    if(this.state.colorModal === 'closed'){
-      this.setState({colorModal: 'open', colorTemp : this.state.color});
-      } else { 
-        this.setState({colorModal: 'closed'});
-      }
-  }
-
-  handleChangeComplete = (color) => {
-    this.setState({color: color.hex});
-  }
-
-  handleAccept = () => {
-    this.setState({colorModal: 'closed'});
-  }
-
-  handleCancel = () => {
-    this.setState({
-      colorModal: 'closed',
-      color : this.state.colorTemp
-    });
-  }
+  cube.position.x = newPosition;
+  cube.position.z = -2.5;
+  cube.name = 'mesh_'+layer.naam;
 
 
-  addCube(){
-    
-    var layerList = this.props.layerListdata
-    var myCubes = []
+  myCubes.push(cube)
+  // console.log(myCubes)
 
-    //reset scene from cubes
-    this.scene = '';
-    this.scene = new THREE.Scene();
+  myCubes.map((Cube_i)=>{
+    this.scene.add( Cube_i );
+  })
+  console.log(this.scene.children)
+
+  // this.setState({
+  //   cubes: myCubes
+  // });
+
+  // console.log(this.state.cubes)
+})
+
+}
+
+
+//omgeving bepalen volgens three.js -> scene, camera, licht
+
+componentDidMount() {
+
+    this.scene = new THREE.Scene()
     var canvasWidth = document.getElementById('canvas').clientWidth;
-    var camera = new THREE.PerspectiveCamera( 75, canvasWidth/window.innerHeight, 0.1, 1000 );
+    var camera = new THREE.PerspectiveCamera( 75, canvasWidth/window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer();
 
-    var light = new THREE.HemisphereLight( 0xffffff, 0x080820, .5 );
-    this.scene.add( light );
-    var ambientLight = new THREE.AmbientLight( 0xffffff, .5 );
+    var light = new THREE.HemisphereLight( 0xffffff, 0x080820, .5);
+    this.scene.add( light);
+    var ambientLight = new THREE.AmbientLight(0xffffff, .5);
     this.scene.add( ambientLight );
     var directionalLight = new THREE.DirectionalLight( 0xffffff, .3 );
     this.scene.add( directionalLight );
@@ -81,92 +139,61 @@ class Cube extends Component {
     directionalLight.position.set(0,0,200);
 
     renderer.setSize( canvasWidth, window.innerHeight );
-    
-    //orbit-controls
+
+    //orbit-controles
 
     var controls = new OrbitControls( camera, renderer.domElement );
 
-    //controls.update() must be called after any manual changes to the camera's transform
     camera.position.set( -2, 2, 6 );
     controls.update();
 
-    // document.body.appendChild( renderer.domElement );
-    // use ref as a mount point of the Three.js scene instead of the document.body
-    this.mount.appendChild( renderer.domElement );
+    this.mount.appendChild ( renderer.domElement );
 
     camera.position.z = 150;
-    var animate = () => {
+    var animate =() => {
       requestAnimationFrame( animate );
-      // required if controls.enableDamping or controls.autoRotate are set to true
       controls.update();
       renderer.render( this.scene, camera );
     };
     animate();
+}
 
-    layerList.map((layer, index) =>{
+//Hoe wordt het getoond...
+render() {
  
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshPhongMaterial( { color: '#ff5000' } );
-    var cube = new THREE.Mesh( geometry, material );
-    let newWidth = layer.dikte
- 
-    cube.scale.x = newWidth;
-    cube.scale.y = 100;
-    cube.scale.z = 100;
-    let newPosition = 0;
-    if(this.state.cubes.length > 0){
-      let lastBox = this.state.cubes.length -1;
-      newPosition = this.state.cubes[lastBox].position.x + (this.state.cubes[lastBox].scale.x/2) + (newWidth/2);
-    }
-
-    cube.position.x = newPosition;
-    cube.position.z = -2.5;
-    cube.name = 'mesh_' + (this.state.cubes.length + 1);
-    this.scene.add( cube );
-
-    myCubes.push(cube)
-    myCubes.map((Cube_i) =>{
-      this.scene.add( Cube_i)
-    })
-    })
-  }
-
-
-  render() {
-    return (
-      <div className="container">
-        <div className="control-bar">
-          <div className="controls">
-            {this.listItems(this.state.cubes)}
-            <div className={'color-picker-modal ' + this.state.colorModal}>
-              <PhotoshopPicker
-              color={ this.state.color}
-              onChangeComplete= {this.handleChangeComplete}
-              onAccept={this.handleAccept}
-              onCancel={this.handleCancel}/>
-            </div>
-            <div
-              className="colorPicker"
-              style={{backgroundColor: this.state.color}}
-              onClick= {this.handleColorModalOpen}/>
-
-            <button onClick={this.addCube}>Update 3D-view</button>
-          </div>
-        </div>
-        <div className="canvas" id="canvas">
-          <div ref={ref => (this.mount = ref)} />
+  return (
+    <div className= "container">
+      <div className= "control-bar">
+        <div className= "controls">
+          <button onClick={()=>this.addCube()}>Update 3D-view</button>
         </div>
       </div>
+      <div className="canvas" id="canvas">
+        <div ref={ref => (this.mount = ref)}/>
+      </div>
+    </div>
+  )
+  
 
-    )
-  }
+}
+
+
+
+
 }
 
 const mapReduxStateToProps = reduxState => ({
+
   layerListdata: reduxState.layerList.layerList,
+
 })
 
 export default connect(mapReduxStateToProps)(Cube);
 
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Cube />, rootElement);
+ReactDOM.render(<Cube />, rootElement)
+
+
+
+
---
+++ src/Components/Colorpicker.js
@@ -0,0 +1,139 @@
+import './Cube.css';
+import React, { Component } from 'react';
+import {PhotoshopPicker} from 'react-color';
+import {saveLayerList} from '../redux/actions/userAction';
+import {connect} from 'react-redux'
+
+
+var layerIndex = 0
+
+
+class Colorpicker extends Component {
+  constructor(props) {
+    super(props);
+    this.scene = '';
+    this.state = {
+      cubes : [],
+      color : '#FFFFFF',
+      colorTemp : '#FFFFFF',
+      colorModal : 'closed'
+    }
+
+
+}
+
+
+
+//color-picker -> open en dicht, basiskleur
+  handleColorModalOpen = () => {
+    if(this.state.colorModal === 'closed') {
+      this.setState({colorModal: 'open', colorTemp : this.state.color});
+    } else {
+      this.setState({colorModal: 'closed'})
+    }
+  }
+
+
+
+    handleChangeComplete = (color) => {
+      this.setState({ color: color.hex });
+    }
+
+
+
+
+    handleAccept = () => {
+      this.setState({colorModal: 'closed'});
+
+
+      //set current layerindex
+      var layerList = this.props.layerListdata
+      var currentLayerId = this.props.layerdata
+      function searchElement(list){
+        return list.naam === currentLayerId
+      }
+      if (layerList.indexOf(layerList.find(searchElement))!==-1) {
+        layerIndex = layerList.indexOf(layerList.find(searchElement))
+      }
+
+      //set layer color
+      console.log(this.state.color)
+      layerList[layerIndex].kleur = this.state.color
+
+
+      //save in layerlist
+      this.props.saveLayerList(layerList)
+      console.log('updated layerList is:',layerList)
+    }
+
+
+
+
+
+    handleCancel = () => {
+      this.setState ({
+        colorModal: 'closed',
+        color : this.state.colorTemp
+      });
+    }
+
+
+//Hoe wordt het getoond...
+render() {
+ 
+  return (
+    <div className= "container">
+      <div className= "control-bar">
+        <div className= "controls">
+        
+        <div className={'color-picker-modal ' + this.state.colorModal }>
+            
+        {/* pop-up vester */}
+            <PhotoshopPicker
+                color= {this.state.color}
+                onChangeComplete = {this.handleChangeComplete}
+                onAccept = {this.handleAccept}
+                onCancel = {this.handleCancel}/>
+
+        </div>
+
+        {/* bol met kleur */}
+        <div className = "colorPicker"
+          style = {{backgroundColor: this.state.color}}
+          onClick = {this.handleColorModalOpen}/>
+        
+        </div>
+
+      </div>
+      
+        <div className="canvas" id="canvas">
+        <div ref={ref => (this.mount = ref)}/>
+
+      </div>
+    </div>
+  )
+  
+
+}
+
+
+
+
+}
+
+const mapReduxStateToProps = reduxState => ({
+
+  layerListdata: reduxState.layerList.layerList,
+  layerdata: reduxState.layer.layer
+
+})
+
+export default connect(mapReduxStateToProps, {saveLayerList})(Colorpicker);
+
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<Colorpicker />, rootElement)
+
+
+
+
